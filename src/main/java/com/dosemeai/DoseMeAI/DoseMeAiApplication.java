7df9a2f181e495a587d4440e477160643da00acd
Your Name <victor.lopesvfk11@gmail.com>
2025-09-28 16:09:48 -0300

    first commit

---
+++ src/main/java/com/dosemeai/DoseMeAI/DoseMeAiApplication.java
@@ -0,0 +1,13 @@
+package com.dosemeai.DoseMeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoseMeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoseMeAiApplication.class, args);
+	}
+
+}
